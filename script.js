7d20f0b3b83f443ee928a38d7f5a66296ebf7ed2
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-04 11:59:48 +0300

    "Methods"

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const points = [70, 75, 25, 63, 15, 5, 99, 12];
-// const names = ["jack", "jane", "aaron", "zack"];
-
-// const resultFind = names.reverse();
-
-const resultPoint = points.sort((a, b) => b - a);
-
-console.log(resultPoint);
-
 const students = [
   {
     name: "jack",
@@ -24,18 +15,24 @@ const students = [
     name: "kyle",
     score: 45,
   },
+  {
+    name: "charlie",
+    score: 77,
+  },
+  {
+    name: "sean",
+    score: 83,
+  },
 ];
 
-const result_1 = students.sort((a, b) => {
-  if (a.score > b.score) {
-    return -1;
-  } else if (b.score > a.score) {
-    return 1;
-  } else {
-    return 0;
-  }
-});
+// const filtered = students.filter((item) => item.score < 50);
 
-const result_2 = students.sort((a, b) => b.score - a.score);
+// const plusPoint = filtered.map(
+//   (item) => `${item.name}'s updated score is ${item.score + 10}.`
+// );
 
-console.log(result_2);
+const newScores = students
+  .filter((item) => item.score < 50)
+  .map((item) => `${item.name}'s updated score is ${item.score + 10}.`);
+
+console.log(newScores);
