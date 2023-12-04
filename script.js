456afecc8bbe51bf6ec3ec1a19e6af07d20cb215
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-04 09:06:08 +0300

    "findIndex()"

--- script.js
+++ script.js
@@ -2,48 +2,28 @@ const points = [70, 75, 25, 63, 15, 5, 99, 12];
 
 const resultFind = points.find((point) => point > 50);
 
-console.log(resultFind);
+// console.log(resultFind);
 
-// const students = [
-//   {
-//     name: "jack",
-//     score: 40,
-//   },
-//   {
-//     name: "mary",
-//     score: 60,
-//   },
-//   {
-//     name: "angela",
-//     score: 35,
-//   },
-//   {
-//     name: "kyle",
-//     score: 45,
-//   },
-//   {
-//     name: "jack",
-//     score: 75,
-//   },
-//   {
-//     name: "mary",
-//     score: 80,
-//   },
-//   {
-//     name: "angela",
-//     score: 65,
-//   },
-//   {
-//     name: "kyle",
-//     score: 85,
-//   },
-// ];
+const students = [
+  {
+    name: "jack",
+    score: 40,
+  },
+  {
+    name: "mary",
+    score: 60,
+  },
+  {
+    name: "angela",
+    score: 35,
+  },
+  {
+    name: "kyle",
+    score: 45,
+  },
+];
 
-// const studentReduce = students.reduce((value, student) => {
-//   if (student.name === "jack") {
-//     value += student.score / 2;
-//   }
-//   return value;
-// }, 0);
+const studentIndex = students.findIndex((item) => item.name === "kyle");
+students[studentIndex].name = "bill";
 
-// console.log(studentReduce);
+console.log(students);
