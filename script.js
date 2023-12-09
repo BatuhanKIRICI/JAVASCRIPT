0b738f853c42b6bd19861b721fa4c459a1d6b3a1
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-09 15:42:08 +0300

    "Delete element"

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-function getMiddle(s) {
-  //Code goes here!
-  let long = s.length;
-  let middle = Math.floor(long / 2);
-  if (long % 2 === 1) {
-    return s[middle];
-  } else if (long % 2 === 0) {
-    return s[middle - 1] + s[middle];
-  }
-}
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
 
-console.log(getMiddle("janet"));
+const generateTemplate = (todo) => {
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+  list.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  // console.log(todo);
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
