4d6e75d9ab53a131b906e61f4baed6ecc6e832ad
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-11 18:12:38 +0300

    "Callback function"

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const request = new XMLHttpRequest();
+const getTodos = (callBack) => {
+  const request = new XMLHttpRequest();
 
-request.addEventListener("readystatechange", () => {
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText);
-  } else if (request.readyState === 4) {
-    console.log("Not successful!");
-  }
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      // console.log(request.responseText);
+      callBack(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      // console.log("Not successful!");
+      callBack("Not successful!", undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
+  request.send();
+};
+
+getTodos((err, data) => {
+  // console.log(err, data);
+  err ? console.log(err) : console.log(data);
 });
-
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
-request.send();
