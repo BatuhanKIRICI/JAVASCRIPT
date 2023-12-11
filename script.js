442e1ee6502d9b6e303b7de8ed00e6ef92832ea7
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-11 18:20:41 +0300

    "JSON parse"

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ const getTodos = (callBack) => {
 
   request.addEventListener("readystatechange", () => {
     if (request.readyState === 4 && request.status === 200) {
-      // console.log(request.responseText);
-      callBack(undefined, request.responseText);
+      const info = JSON.parse(request.responseText);
+
+      callBack(undefined, info);
     } else if (request.readyState === 4) {
-      // console.log("Not successful!");
       callBack("Not successful!", undefined);
     }
   });
 
-  request.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos");
   request.send();
 };
 
 getTodos((err, data) => {
-  // console.log(err, data);
   err ? console.log(err) : console.log(data);
 });
