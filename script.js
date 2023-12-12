64988b9fbe6665743866a3ee197741d509fdddea
BatuhanKIRICI <k.sukr48@gmail.com>
2023-12-12 08:12:24 +0300

    "Async Await"

--- script.js
+++ script.js
@@ -2,7 +2,19 @@
   .then((response) => response.json())
   .then((json) => console.log(json)); */
 
-fetch("/json/first.json")
+/* fetch("/json/first.json")
   .then((response) => response.json())
   .then((data) => console.log(data))
-  .catch((err) => console.log(data));
+  .catch((err) => console.log(err)); */
+
+getTodos = async () => {
+  let response = await fetch("/jsoscn/first.json");
+  response.status !== 200 && console.log("Wrong endpoint!");
+  const data = await response.json();
+  return data;
+};
+
+getTodos()
+  .then((response) => response)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
