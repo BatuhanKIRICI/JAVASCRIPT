caec124e56c5296b3f20468c0648eae441c80a43
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-20 17:43:15 +0200

    "Variable types"

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Hello!")
+let num = [0, 1, 2, "3", null]
+
+const calculate_age = () => {
+    return 0
+}
+
+
+
+console.log(typeof num)
+
+console.log(typeof calculate_age)
+
+console.log(typeof calculate_age())
