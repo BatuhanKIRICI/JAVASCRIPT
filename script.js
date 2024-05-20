ef0265d88420ec9e22d0c87a4ea1222e1a9edb14
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-20 22:03:47 +0200

    Trial for git push

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-let val = 15
-let array = [1, 2131, 4]
-Number(array)
-
-console.log(val.toFixed(5))
-console.log(typeof val.toString())
-console.log(array)
-console.log(typeof array)
+console.log("Trial for git push")
