fd3bb938725f3a31653a91f9da337fedd3cb39b7
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-24 14:55:24 +0200

    "Day calculation 2"

--- script.js
+++ script.js
@@ -5,7 +5,11 @@
 
 let firstDate = new Date("5/31/1993")
 let secondDate = new Date("5/31/2024")
+let dateNow = Date.now()
 
-marginDate = (secondDate - firstDate) / 1000 / 60 / 60 / 24
+marginDate_1 = (dateNow - firstDate) / 1000 / 60 / 60 / 24
 
-console.log(`${marginDate} days have passed till this day!`)
+marginDate_2 = (secondDate - firstDate) / 1000 / 60 / 60 / 24
+
+console.log(`${marginDate_1} days have passed!`)
+console.log(`${marginDate_2} days will have passed till that day!`)
