7dc3c18101717d58dc489b5bb39ee1dd7726013f
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-06-06 17:19:57 +0200

    "Object literals"

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-let arr = ["BMW", "VW", "Mercedes", "Audi"]
+let person = {
+    firstName: "Michael",
+    lastName: "Schmidt",
+    getFullName: function () {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
 
-arr[arr.length] = "Opel"
-
-console.log(arr)
-
-// arr[0] = "Bugatti"
-
-// console.log(arr)
-
-arr.push("Bugatti")
-arr.unshift("Ferrari")
-
-console.log(arr)
-
-arr.reverse()
-
-console.log(arr)
-
-arr.sort()
-
-console.log(arr)
-
-let str = "Ford,Seat"
-
-let arr2 = str.split(",")
-
-arr = arr.concat(arr2)
-
-console.log(arr)
+console.log(person.getFullName())
