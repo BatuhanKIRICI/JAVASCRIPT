96bab08efb1c05b3ad3e3e539e3ae75d1fc5175d
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-06-12 13:48:16 +0200

    "for loop"

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let person = {
-    firstName: "Michael",
-    lastName: "Schmidt",
-    getFullName: function () {
-        return `${this.firstName} ${this.lastName}`
+
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i === 6) {
+//         console.log("My favorite number: " + i)
+//     }
+//     else {
+//         console.log(i)
+//     }
+// }
+
+// let total = 0
+// for (i = 0; i < 10; i++) {
+//     total += i
+// }
+
+// console.log(total)
+
+for (i = 0; i < 10; i++) {
+    for (j = 0; j < 10; j++) {
+        console.log(`i = ${i} ; j = ${j}`)
     }
 }
-
-console.log(person.getFullName())
