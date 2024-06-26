772c367ac720a223d427cc3837fa0c37c8e00790
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-06-26 16:40:16 +0200

    "Number guessing game"

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-let cars = ["BMW", "Mercedes", "Audi", "Opel"]
+let round = Number(prompt("Round number?"))
+let option = round
 
-// for (let i in cars) {
-//     console.log(`index : ${i} ; value : ${cars[i]}`)
-// }
+let guess
+let counter = 0
 
-// cars.forEach((item) => {
-//     console.log(item)
-// })
+let num = Math.floor(Math.random() * 10 + 1)
 
-let newCars = cars.map((item) => {
-    return item
-})
 
-console.log(newCars)
+while (round > 0) {
+
+    round--
+    counter++
+
+    guess = Number(prompt("Choose a number: "))
+
+    if (guess === num) {
+        console.log(`Yes! You've done it in ${counter}th round! Your score is ${100 - (counter - 1) * (100 / option)}!`)
+        break
+    } else if (guess > num) {
+        console.log("Down")
+    } else {
+        console.log("Up")
+    }
+
+    if (round === 0) {
+        console.log(`Game over! Number is: ${num}`)
+        break
+    }
+}
