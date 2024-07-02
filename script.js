90a990e265de23acb3cc8fee5afb515ab6c69ac8
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-02 20:30:04 +0200

    "localStorage && sessionStorage"

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function letterFinder(word, match) {
+const firstName = localStorage.setItem("firstName", "Batuhan")
+const lastName = localStorage.setItem("lastName", "Kirici")
 
-    for (var i = 0; i < word.length; i++) {
+let val = localStorage.getItem("firstName")
 
-        if (word[i] === match) {
-            console.log('Found the', match, 'at', i + 1);
-        } else {
-            console.log('---No match found at', i + 1);
-        }
-    }
-}
+console.log(val)
+console.log(localStorage)
 
+localStorage.removeItem("firstName")
+// localStorage.removeItem("lastName")
+
+localStorage.clear()
+
+let hobbies = ["sport", "music", "programming"]
+
+localStorage.setItem("hobbies", JSON.stringify(hobbies))
+
+let val_2 = JSON.parse(localStorage.getItem("hobbies"))
+
+console.log(...val_2)
+
+// const city = sessionStorage.setItem("city", "Kusel")
+// const country = sessionStorage.setItem("country", "Germany")
+
+// console.log(sessionStorage)
