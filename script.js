d6ccb0eb32990fb4c9d96b546021868018ce2af0
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-03 23:08:01 +0200

    "match()"

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-// try {
-//     console.log(a + b)
-// } catch (err) {
-//     console.log(err)
-//     console.log("There was an error!")
-//     console.log("The error was saved in the error log.")
-// }
-// console.log("This program never stops.")
+let letters = "abc"
 
-try {
-    throw new ReferenceError()
-} catch (err) {
-    console.log(err)
-    console.log("There was a Reference Error!")
-}
-console.log("This program never stops.")
+console.log(letters.match(/b/))
+
+console.log(letters.match(/d/))
+
