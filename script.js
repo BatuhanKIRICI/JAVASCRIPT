b071f7e9b845b1a9cf2afa03b072d89a6253444b
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-03 22:51:02 +0200

    "ReferenceError()"

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let a = 0
-let c = ""
-
+// try {
+//     console.log(a + b)
+// } catch (err) {
+//     console.log(err)
+//     console.log("There was an error!")
+//     console.log("The error was saved in the error log.")
+// }
+// console.log("This program never stops.")
 
 try {
-    console.log(a + b + c)
+    throw new ReferenceError()
 } catch (err) {
     console.log(err)
+    console.log("There was a Reference Error!")
 }
+console.log("This program never stops.")
