8a3cd64ff00cd50e5af97e20b46aa8cca5825183
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-03 23:52:06 +0200

    "letterFinder() with conditionals"

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-function addTwoNums(a, b) {
-    try {
-        if (typeof a !== "number") {
-            throw new ReferenceError("the first argument is not a number")
+function letterFinder(word, match) {
 
-        } else if (typeof b !== "number") {
-            throw new ReferenceError("the second argument is not a number")
-        } else {
-            console.log(a + b)
+    let condition1 = typeof (word) == 'string' && word.length >= 2
+
+    let condition2 = typeof (match) == 'string' && match.length === 1
+
+    if (condition1 === true && condition2 === true) {
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] == match) {
+                console.log('Found the', match, 'at', i + 1)
+            } else {
+                console.log('---No match found at', i + 1)
+            }
         }
-    } catch (err) {
-        console.log("Error!", err)
+    } else {
+        console.log("Please pass correct arguments to the function.")
     }
-    console.log("It still works")
 }
 
-addTwoNums(5, "5")
+letterFinder(45, 4)
+
+letterFinder("cat", "c")
+
