1d618a0919cbd3f38684d2ac1fb753a86f29630a
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-03 22:43:57 +0200

    "try && catch"

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-const firstName = localStorage.setItem("firstName", "Batuhan")
-const lastName = localStorage.setItem("lastName", "Kirici")
+let a = 0
+let c = ""
 
-let val = localStorage.getItem("firstName")
 
-console.log(val)
-console.log(localStorage)
-
-localStorage.removeItem("firstName")
-// localStorage.removeItem("lastName")
-
-localStorage.clear()
-
-let hobbies = ["sport", "music", "programming"]
-
-localStorage.setItem("hobbies", JSON.stringify(hobbies))
-
-let val_2 = JSON.parse(localStorage.getItem("hobbies"))
-
-console.log(...val_2)
-
-// const city = sessionStorage.setItem("city", "Kusel")
-// const country = sessionStorage.setItem("country", "Germany")
-
-// console.log(sessionStorage)
+try {
+    console.log(a + b + c)
+} catch (err) {
+    console.log(err)
+}
