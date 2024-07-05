0a985fd1c462e3b7261ff91e10b7922a5dc020c2
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-05 10:54:36 +0200

    "Object create"

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-function consoleStyler(color, background, fontSize, txt) {
-    var message = "%c" + txt;
-    var style = `color: ${color};`
-    style += `background: ${background};`
-    style += `font-size: ${fontSize};`
-    console.log(message, style);
+let car = {
+    engineV8: true,
+    colorWhite: true,
+    sport: true
 }
 
-consoleStyler('#1d5c63', '#ede6db', '40px', 'Congrats!');
+let newCar = Object.create(car)
 
-function celebrateStyler(reason) {
-    var fontStyle = "color: tomato; font-size: 50px"
-    if (reason == "birthday") {
-        console.log(`%cHappy birthday`, fontStyle);
-    } else if (reason == "champions") {
-        console.log(`%cCongrats on the title!`, fontStyle);
-    } else {
-        console.log(fontStyle);
-    }
-}
-
-celebrateStyler('birthday');
-
-consoleStyler('#1d5c63', '#ede6db', '40px', 'Congrats!')
-celebrateStyler("birthday")
-
-function styleAndCelebrate(color, background, fontSize, txt, reason) {
-    consoleStyler(color, background, fontSize, txt);
-    celebrateStyler(reason);
-}
-
-styleAndCelebrate('ef7c8e', 'fae8e0', '30px', 'You made it!', 'champions')
-
+console.log(newCar)
