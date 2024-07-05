dfd849703a25206f0db3b5aa312ec0be393b9a71
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-05 13:16:24 +0200

    "Function constructor in ES5"

--- script.js
+++ script.js
@@ -1,42 +1,13 @@
-class Person {
-    constructor(name = "Tom", age = 20, energy = 100) {
-        this.name = name,
-            this.age = age,
-            this.energy = energy
-    }
-    sleep() {
-        this.energy += 10
-    }
-    doSomething() {
-        this.energy = -10
+function Person(name, yearOfBirth, job) {
+    this.name = name
+    this.yearOfBirth = yearOfBirth
+    this.job = job
+    this.calculateAge = () => {
+        return 2024 - this.yearOfBirth
     }
 }
 
-class Worker extends Person {
-    constructor(name = "Tom", age = 20, energy = 100, xp = 0, hourlyWage = 10) {
-        super(name, age, energy);
-        this.xp = xp;
-        this.hourlyWage = hourlyWage;
-    }
-    goToWork() {
-        this.xp += 10;
-    }
-}
+let man = new Person("Karl", 1990, "engineer")
 
-function intern() {
-    let newIntern = new Worker("Bob", 21, 110);
-    newIntern.goToWork();
-    return newIntern;
-}
-
-function manager() {
-    let newManager = new Worker("Alice", 30, 120, 100, 30);
-    newManager.doSomethingFun();
-    return newManager;
-}
-
-let internObject = intern();
-console.log(`Intern: Name=${internObject.name}, Age=${internObject.age}, Energy=${internObject.energy}, XP=${internObject.xp}, HourlyWage=${internObject.hourlyWage}`);
-
-let managerObject = manager();
-console.log(`Manager: Name=${managerObject.name}, Age=${managerObject.age}, Energy=${managerObject.energy}, XP=${managerObject.xp}, HourlyWage=${managerObject.hourlyWage}`);
+console.log(man)
+console.log(man.calculateAge())
