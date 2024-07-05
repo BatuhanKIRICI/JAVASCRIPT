a2d202c6591efc781d7944bdae8d1dacaa254906
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-05 11:30:07 +0200

    "Constructor example"

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-let car = {
-    engineV8: true,
-    colorWhite: true,
-    sport: true
+class Person {
+    constructor(name = "Tom", age = 20, energy = 100) {
+        this.name = name,
+            this.age = age,
+            this.energy = energy
+    }
+    sleep() {
+        this.energy += 10
+    }
+    doSomething() {
+        this.energy = -10
+    }
 }
 
-let newCar = Object.create(car)
+class Worker extends Person {
+    constructor(name = "Tom", age = 20, energy = 100, xp = 0, hourlyWage = 10) {
+        super(name, age, energy);
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+    }
+    goToWork() {
+        this.xp += 10;
+    }
+}
 
-console.log(newCar)
+function intern() {
+    let newIntern = new Worker("Bob", 21, 110);
+    newIntern.goToWork();
+    return newIntern;
+}
 
-let anotherCar = Object.create(car)
+function manager() {
+    let newManager = new Worker("Alice", 30, 120, 100, 30);
+    newManager.doSomethingFun();
+    return newManager;
+}
 
-anotherCar.colorWhite = false
+let internObject = intern();
+console.log(`Intern: Name=${internObject.name}, Age=${internObject.age}, Energy=${internObject.energy}, XP=${internObject.xp}, HourlyWage=${internObject.hourlyWage}`);
 
-console.log(anotherCar)
+let managerObject = manager();
+console.log(`Manager: Name=${managerObject.name}, Age=${managerObject.age}, Energy=${managerObject.energy}, XP=${managerObject.xp}, HourlyWage=${managerObject.hourlyWage}`);
