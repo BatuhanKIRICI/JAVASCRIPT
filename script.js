585aa1bc692c3c67ccb5083560d707cfab3b4cc8
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-05 21:50:56 +0200

    "Object.entries()-(keys,values)"

--- script.js
+++ script.js
@@ -1,46 +1,10 @@
-function Employee(name, salary) {
-
-    if (!(this instanceof Employee)) {
-        return new Employee(name, salary)
-    }
-
-    this.name = name
-    this.salary = salary
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
 }
 
-Employee.prototype.calculateTax = function () {
-    let month = new Date().getMonth() + 1
-    let total = this.salary * month
-    let tax = 0
-    let paid = 0
-
-
-    if (total < 20000) {
-
-        tax = total * 0.2
-
-    } else if (total > 20000 && total < 30000) {
-
-        tax = total * 0.25
-
-    } else {
-
-        tax = total * 0.25
-    }
-    return {
-        tax: tax,
-        paid: total - tax
-    }
+for (const key of Object.entries(clothingItem)) {
+    console.log(key, ":", clothingItem[key])
 }
-
-let emp1 = new Employee("Friedrich", 2000)
-
-console.log(emp1)
-console.log(emp1.calculateTax())
-
-console.log("***************************************************")
-
-let emp2 = Employee("Mark", 4000)
-
-console.log(emp2)
-console.log(emp2.calculateTax())
