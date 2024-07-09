f74c20a172a50a5e02a20f90bbb061bc27ecd910
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-09 22:58:47 +0200

    "Immediate function"

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-let val;
+(function (name) {
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let today = new Date().getDay();
 
-function MultiplyByTwo(a, b, c, d_callback) {
-  let arr = [];
-  if (d_callback && typeof d_callback === "function") {
-    for (let i = 0; i < 3; i++) {
-      arr[i] = d_callback(arguments[i] * 2);
-    }
-  }
-
-  return arr;
-}
-
-val = MultiplyByTwo(2, 5, 10, addOne);
-
-console.log(val);
-
-function addOne(a) {
-  return a + 1;
-}
-
-//Anonymous function
-
-val = MultiplyByTwo(4, 6, 12, function (a) {
-  return a * 5;
-});
-
-console.log(val);
+  let greet = `Hello ${name}! Today is ${days[today]}.`;
+  console.log(greet);
+})("Andrea");
