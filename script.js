ccc6ca0375d0c6816177bf5f875572bc8b73c3e8
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-09 22:49:46 +0200

    "Callback and Anonymous functions"

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-let Person = function (name) {
-  this.name = name;
-};
+let val;
 
-Person.prototype.Introduce = function () {
-  console.log(this.name);
-};
+function MultiplyByTwo(a, b, c, d_callback) {
+  let arr = [];
+  if (d_callback && typeof d_callback === "function") {
+    for (let i = 0; i < 3; i++) {
+      arr[i] = d_callback(arguments[i] * 2);
+    }
+  }
 
-let Teacher = function (name, branch) {
-  Person.call(this, name);
-  this.branch = branch;
-};
+  return arr;
+}
 
-Teacher.prototype = Object.create(Person.prototype);
-Teacher.prototype.constructor = Teacher;
-Teacher.prototype.teach = function () {
-  console.log(`I teach ${this.branch}. `);
-};
+val = MultiplyByTwo(2, 5, 10, addOne);
 
-let p1 = new Person("Mike");
+console.log(val);
 
-let p2 = new Teacher("Mark", "Literature");
+function addOne(a) {
+  return a + 1;
+}
 
-p1.Introduce();
-p2.Introduce();
-p2.teach();
+//Anonymous function
+
+val = MultiplyByTwo(4, 6, 12, function (a) {
+  return a * 5;
+});
+
+console.log(val);
