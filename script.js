eec690e5f6263531c6ead6749417443f5bfb2ced
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-11 23:50:45 +0200

    "Numeric Range app with call(), apply() and bind()"

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-(function (name) {
-  let days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
-  let today = new Date().getDay();
+let numeric = {
+  min: 0,
+  max: 100,
+  checkNumericRange: function (value) {
+    if (typeof value !== "number") {
+      return false;
+    } else {
+      return value >= this.min && value <= this.max;
+    }
+  },
+};
 
-  let greet = `Hello ${name}! Today is ${days[today]}.`;
-  console.log(greet);
-})("Andrea");
+// console.log(numeric.checkNumericRange(-20));
+// console.log(numeric.checkNumericRange(20));
+
+let num1 = { min: 10, max: 58 };
+
+console.log(numeric.checkNumericRange.call(num1, 43));
+console.log(numeric.checkNumericRange.call(num1, 77));
+console.log("**************************************************");
+console.log(numeric.checkNumericRange.apply(num1, [12]));
+console.log(numeric.checkNumericRange.apply(num1, [95]));
+console.log("**************************************************");
+
+let checkNumber = numeric.checkNumericRange.bind(num1);
+
+console.log(checkNumber(56));
