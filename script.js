3355c1845493e18ed30ffb22491f831a69d11f1f
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-14 12:32:29 +0200

    "Spread operator"

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-// let list = {
-//   category: "car",
-//   brands: ["Opel", "Mercedes", "Audi", "BMW"],
-//   call: function () {
-//     this.brands.map((name) => console.log(`${this.category} ${name}`));
-//   },
-// };
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = ["a", "b", "c", "d"];
 
-// list.call();
+arr1.push(...arr2);
 
-function Game() {
-  this.live = 0;
-  this.addLive = () => {
-    this.oneUp = setInterval(() => console.log(this.live++), 1000);
-  };
-}
+console.log(arr1);
 
-let player = new Game();
-player.addLive();
+console.log("/////////////////////////////////////////////////");
+
+arr1.push(...arr1);
+
+console.log(arr1);
+
+console.log("/////////////////////////////////////////////////");
+
+arr1.unshift(...arr2);
+
+console.log(arr1);
+
+console.log("/////////////////////////////////////////////////");
+
+let arr3 = ["x", "y", ...arr2, arr1];
+
+console.log(arr3);
