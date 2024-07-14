3f0a32296823250cea30155c297a01249632a09d
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-14 12:19:52 +0200

    "this - in arrow functions"

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-const multiply = (x, y) => x * y;
+// let list = {
+//   category: "car",
+//   brands: ["Opel", "Mercedes", "Audi", "BMW"],
+//   call: function () {
+//     this.brands.map((name) => console.log(`${this.category} ${name}`));
+//   },
+// };
 
-console.log(multiply(4, 5));
+// list.call();
 
-console.log("*************************************");
+function Game() {
+  this.live = 0;
+  this.addLive = () => {
+    this.oneUp = setInterval(() => console.log(this.live++), 1000);
+  };
+}
 
-const splittinFunc = (text) => text.split(" ");
-
-console.log(splittinFunc("Hello World again and again and..."));
-
-console.log("*************************************");
-
-const objMaker = (name, id) => ({
-  name,
-  id,
-});
-
-console.log(objMaker("Luke", 45566));
-
-console.log("*************************************");
-
-const arr = [1, 651, 2, 546, 489, 7813, 456, 75, 46, 36, 99, 56151, 77, 88];
-
-const arr_filter = arr.filter((x) => x % 2 === 0);
-
-console.log(arr_filter);
+let player = new Game();
+player.addLive();
